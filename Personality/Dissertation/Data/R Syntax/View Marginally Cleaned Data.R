c5d520a1b7d15d957b9a98c8c36f2d65c73e67ab
Bryan Bunn <bbunn1@its.nicholls.edu>
2018-08-14 14:50:00 -0500

    Moved file into new papaja format for SIOP

--- Personality/Dissertation/Data/R Syntax/View Marginally Cleaned Data.R
+++
@@ -1,4 +0,0 @@
-#View "Marginally Cleaned Data". File contains cleaned data and also missing cases.
-setwd("~/Documents/Research Projects/Personality/Dissertation/Data")
-data <- read.csv("/Users/ccasti02/Documents/Research Projects/Personality/Dissertation/Data/Marginally Cleaned Data.csv")
-View(data)
